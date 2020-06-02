4d1c320b354dc4c25ecf565c70dc6d64a6fc75b6
anabaral <anabaral@gmail.com>
2020-06-02 18:21:40 +0900

    Create install-metrics-server-202003.sh

---
+++ vbox_kube/install-metrics-server-202003.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+# this is old versioned
+#
+git clone https://github.com/kubernetes-sigs/metrics-server
+cd metrics-server
+sed -i -e 's/^          - --secure-port=4443/&\n          - --kubelet-insecure-tls/' deploy/kubernetes/metrics-server-deployment.yaml
+kubectl apply -f deploy/kubernetes/metrics-server-deployment.yaml
+echo ######################################################
+echo # if something wrong, restart kubelet of each nodes...
+echo ######################################################
