d2a161e35df55de7c9906fcaa9672bdafeaea93d
anabaral <anabaral@gmail.com>
2020-06-03 14:25:29 +0900

    Update install-metrics-server.sh

--- vbox_kube/install-metrics-server.sh
+++ vbox_kube/install-metrics-server.sh
@@ -7,7 +7,7 @@ cd metrics-server
 sed -i -e 's/^          - --secure-port=4443/&\n          - --kubelet-insecure-tls/' manifests/base/deployment.yaml
 
 echo "the below one thing may throw error, that's okay: 'no matches for kind \"Kustomization\" ... "
-kubectl apply -f manifests/base/
+kubectl apply -k manifests/base/
 
 echo ######################################################
 echo # if something wrong, restart kubelet of each nodes...
